005b704315a20ad8381274e94d6a0b54b502ed60
Vijayasekhar Deepak <Vijayasekhar.Deepak@ducenIT.com>
2020-07-18 13:01:12 +0530

    storage changes

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -132,7 +132,7 @@ class MessageForm extends React.Component {
 
   getFilePath = () => {
     if (this.props.isPrivateChannel) {
-      return `chat/private-${this.state.channel.id}`;
+      return `chat/private/${this.state.channel.id}`;
     } else {
       return `chat/public`;
     }
